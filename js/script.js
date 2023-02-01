13a834b93ef2aeea0e2f54642301265efe07c7eb
tourecodeur <116043808+tourecodeur@users.noreply.github.com>
2023-02-01 23:39:24 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*Refactoriced by Luis Daniel aka Naoko */
+
+let modal = document.getElementById("modal2")
+let modalImg = document.getElementById("gallery-image")
+let captionText = document.getElementById("caption")
+let expandImg
+let clipBoard1 = new ClipboardJS('#copy')
+
+expandImg = function (id) {
+  const img = document.getElementById(id);
+  modal.style.display = "block"
+  modalImg.src = img.src
+  captionText.innerHTML = img.alt
+  const span = document.getElementsByClassName("close")[1];
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+}
+
+function change(src, type) {
+  if (type === 'icon') document.getElementById("main-image").src = src
+  else if (type === 'half') document.getElementById("main-image2").src = src
+  else if (type === 'full') document.getElementById("main-image3").src = src
+  else if (type === 'custom') document.getElementById("main-image4").src = src
+}
+
